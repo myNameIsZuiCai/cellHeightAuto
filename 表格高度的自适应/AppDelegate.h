ff970206020ee34f282be3fd58a1190bbb1a2d13
“shanghaijunheng” <78899500@qq.com>
2016-09-18 16:26:56 +0800

    Initial Commit

---
+++ 表格高度的自适应/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  表格高度的自适应
+//
+//  Created by 上海均衡 on 16/9/18.
+//  Copyright © 2016年 上海均衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 表格高度的自适应/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  表格高度的自适应
+//
+//  Created by 上海均衡 on 16/9/18.
+//  Copyright © 2016年 上海均衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
